092163694e71817d43f55080e2b7ce8a1438a8e0
Yonshisoru <44347209+Yonshisoru@users.noreply.github.com>
2019-08-02 01:03:54 +0700

    Re-Structure of Source Code
    Extract random code in main to method

--- Main.java
+++ Main.java
@@ -6,95 +6,173 @@
 import java.util.Scanner;
 
 public class Main {
+    private static int total_turn = 9;
+    private static String showWinner = "";
     public static void main(String[] args) {
-        while (true) { //Loop of game -> use for replay if player wanted
-            char[][] arr; //Create Empty of character array
-            XO game = new XO(); //Create Object From XO Class
-            int counter = 9; //Amount of turn for playing in game (3*3)
-            Scanner scan = new Scanner(System.in); //Input Class
-            String winner = ""; //Winner label for ended game
-            while (true) { //Loop until someone is winner
+        do {
+            XO game = createXOBoard();
+            while (true) {
                 try {
-                    System.out.println("------------------------------------------");
-                    System.out.println("Turn of " + game.getStatus() + " Player.");
-                    Thread.sleep(500);
-                    game.showMipmap(); //Method show current map
-                    System.out.print("Input your position of area (x,y) [1-3/1-3]:");
-                    String input = scan.next();
+                    printWelcome();
+                    printBar();
+                    printTurn(game);
+                    printBoard(game);
+                    String input = inputPosition();
                     //Check input template (x,y)
-                    if (input.charAt(0) != ('(') || input.charAt(2) != (',') || input.charAt(input.length() - 1) != (')')) {
-                        System.err.println("Input invalid.");
+                    if (checkSyntax(input)) {
+                        printError();
                         continue;
-                    } else { //If it was followed template,Process will continue.
-                        int x = Integer.valueOf(input.substring(1, 2)) - 1;
-                        int y = Integer.valueOf(input.substring(3, 4)) - 1;
-                        System.out.println("You Choose position " + input);
-                        Thread.sleep(500);
-                        arr = game.getMipmap();
-                        if (arr[x][y] == '*') { //If selected box wasn't selected
-                            arr[x][y] = game.getStatus();
-                        } else { //If selected box was selected, Then try again!!
-                            System.err.println("This box is selected!! Try Again.");
-                            continue;
-                        }
-                        counter--; //Reduce amount turn of playing
+                    } else {
+                        int x = extractPositionX(input);
+                        int y = extractPositionY(input);
+                        showInput(input);
 
-                        if (game.getStatus() == 'x') { //Function switch player
-                            game.setStatus('o');
-                        } else {
-                            game.setStatus('x');
-                        }
+                        if (checkSelectedPosition(game, x, y)) continue;
+                        redureTotal_Turn();
+
+                        switchPlayer(game);
                     }
-                    game.check_win(); //Method check player
+
+                    checkWin(game);
                 } catch (Exception e) {
-                    //e.printStackTrace();
-                    System.err.println("Input invalid.");
+                    printError();
                 }
-                if (game.isWin() == true) { //If someone is winner set winner text to player.
-                    winner = String.valueOf(game.isWho_win());
-                    break;
-                } else if (game.isWin() == false && counter <= 0) { //If ended game and no one is winner.
-                    winner = "None";
-                    break;
-                }
+                if (checkDraw(game)) break;
             }
-            game.showMipmap(); //Show current map
+            printBoard(game);
 
             // Function Showing Winner
-            System.out.println("\n##############################");
-            System.out.println("Winner is " + winner);
-            if (winner.equals("None")) { //If no one is winner.
-                System.out.println("!!Draw!!");
-            }
-            System.out.println("##############################\n");
+            printBar();
+            printWinner();
+            printBar();
 
             //Function Playing again
-            System.out.print("Play again? (y/n) :");
-            char replay = scan.next().charAt(0);
-            if(replay=='y'){
-                System.out.println("\n\n\n\n\n\n\n\n\n");
-            }else if(replay=='n'){
-                System.out.println("\nThank you for playing!!\n");
-                break;
-            }else{
-                try {
-                    System.err.println("Input invalid!!");
-                    Thread.sleep(500);
-                    System.err.println(".");
-                    Thread.sleep(1000);
-                    System.err.println("..");
-                    Thread.sleep(1000);
-                    System.err.println("...Stopping process");
-                    Thread.sleep(1000);
-                    System.out.println("\nThank you for playing!!");
-                    break;
-                }catch(Exception e){
-                    e.printStackTrace();
-                }
+            checkReplay();
+            break;
+        } while (true);
+        //Out of Loop
+        printBar();
+        //End of Process
+    }
+
+    private static void printWelcome() {
+        System.out.println("Welcome to XO Game :)");
+    }
+
+    private static void redureTotal_Turn() {
+        total_turn--;
+    }
+
+    private static void checkReplay() {
+        System.out.print("Play again? (y/n) :");
+        Scanner scan = new Scanner(System.in);
+        char replay = scan.next().charAt(0);
+        if(replay=='y'){
+            System.out.println("\n\n\n\n\n\n\n\n\n");
+        }else if(replay=='n'){
+            System.out.println("\nThank you for playing!!\n");
+            return;
+        }else{
+            try {
+                System.err.println("Input invalid!!");
+                Thread.sleep(500);
+                System.err.println(".");
+                Thread.sleep(1000);
+                System.err.println("..");
+                Thread.sleep(1000);
+                System.err.println("...Stopping process");
+                Thread.sleep(1000);
+                System.out.println("\nThank you for playing!!");
+                return;
+            }catch(Exception e){
+                e.printStackTrace();
             }
         }
-        //Out of Loop
-        System.out.print("---------------------------------");
-        //End of Process
+    }
+
+    private static void printWinner() {
+        System.out.println("Winner is " + showWinner);
+        if (showWinner.equals("None")) { //If no one is showWinner.
+            System.out.println("!!Draw!!");
+        }
+    }
+
+    private static boolean checkDraw(XO game) {
+        if (game.isWin() == true) { //If someone is showWinner set showWinner text to player.
+            showWinner = String.valueOf(game.isWho_win());
+            return true;
+        } else if (game.isWin() == false && total_turn <= 0) { //If ended game and no one is showWinner.
+            showWinner = "None";
+            return true;
+        }
+        return false;
+    }
+
+    private static void printError() {
+        System.err.println("Input invalid.");
+    }
+
+    private static void checkWin(XO game) {
+        game.check_win(); //Method check player
+    }
+
+    private static void switchPlayer(XO game) {
+        if (game.getStatus() == 'x') { //Function switch player
+            game.setStatus('o');
+        } else {
+            game.setStatus('x');
+        }
+    }
+
+    private static boolean checkSelectedPosition(XO game, int x, int y) {
+        if (game.getMipmap()[x][y] == '*') { //If selected box wasn't selected
+            game.getMipmap()[x][y] = game.getStatus();
+        } else { //If selected box was selected, Then try again!!
+            System.err.println("This box is selected!! Try Again.");
+            return true;
+        }
+        return false;
+    }
+
+    private static void showInput(String input) {
+        System.out.println("You Choose position " + input);
+    }
+
+    private static int extractPositionY(String input) {
+        return Integer.valueOf(input.substring(3, 4)) - 1;
+    }
+
+    private static int extractPositionX(String input) {
+        return Integer.valueOf(input.substring(1, 2)) - 1;
+    }
+
+    private static boolean checkSyntax(String input){
+        if (input.charAt(0) != ('(') || input.charAt(2) != (',') || input.charAt(input.length() - 1) != (')')) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private static String inputPosition() {
+        System.out.print("Input your position of area (x,y) [1-3/1-3]:");
+        Scanner scan = new Scanner(System.in);
+        return scan.next();
+    }
+
+    private static void printBoard(XO game) {
+        game.showMipmap(); //Method show current map
+    }
+
+    private static void printTurn(XO game) {
+        System.out.println("Turn of " + game.getStatus() + " Player.");
+    }
+
+    private static XO createXOBoard() {
+        return new XO();
+    }
+
+    private static void printBar() {
+        System.out.println("------------------------------------------");
     }
 }
